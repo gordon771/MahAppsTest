8c1c4b436a7ad8bca009a7f868245774ef46fb11
gordon771 <gordon.arand@gmx.de>
2020-10-13 15:53:20 +0200

    not working with centralized style in App.xaml

--- GUILib/WindowBase.cs
+++ GUILib/WindowBase.cs
@@ -7,21 +7,7 @@ namespace GUILib
 	public class WindowBase : MetroWindow
 	{
 		public WindowBase()
-		{
-            ResourceDictionary dictResMain = new ResourceDictionary();
-            ResourceDictionary dictRes = new ResourceDictionary();
-
-            // Define Metro style
-            dictRes.Source = new Uri("pack://application:,,,/MahApps.Metro;component/Styles/Controls.xaml");
-            dictResMain.MergedDictionaries.Add(dictRes);
-            dictRes = new ResourceDictionary();
-            dictRes.Source = new Uri("pack://application:,,,/MahApps.Metro;component/Styles/Fonts.xaml");
-            dictResMain.MergedDictionaries.Add(dictRes);
-            dictRes = new ResourceDictionary();
-            dictRes.Source = new Uri("pack://application:,,,/MahApps.Metro;component/Styles/Themes/Light.Blue.xaml");
-            dictResMain.MergedDictionaries.Add(dictRes);            
-
-            this.Resources = dictResMain;
+		{           
         }
 	}
 }
